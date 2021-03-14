8d0902185d5bab64844ac72e33c3202eaa28d214
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2021-03-14 12:09:06 +0900

    add apply.sh

---
+++ monitoring/apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ClusterName=${CLUSTER_NAME}
+RegionName=${REGION_NAME}
+FluentBitHttpPort='2020'
+FluentBitReadFromHead='Off'
+[[ ${FluentBitReadFromHead} = 'On' ]] && FluentBitReadFromTail='Off'|| FluentBitReadFromTail='On'
+[[ -z ${FluentBitHttpPort} ]] && FluentBitHttpServer='Off' || FluentBitHttpServer='On'
+cat ./cwagent-fluent-bit-quickstart.yaml | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl apply -f - 
